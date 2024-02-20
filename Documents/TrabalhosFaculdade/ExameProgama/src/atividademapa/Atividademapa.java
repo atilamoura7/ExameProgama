3649e88161d0049bbc61bf62827c5e0ed5967f3c
Átila Moura <atilamoura.am@gmail.com>
2024-02-20 16:08:18 -0300

    Sua mensagem de commit

---
+++ Documents/TrabalhosFaculdade/ExameProgama/src/atividademapa/Atividademapa.java
@@ -0,0 +1,15 @@
+package atividademapa;
+
+import java.util.ArrayList;
+
+public class Atividademapa 
+{
+    
+    public static void main(String[] args)
+    {
+        ExameGlicemia exame = new ExameGlicemia();
+//        lista.add(exame);
+//        System.out.println(lista.get(0));
+    }
+    
+}
